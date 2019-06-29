b2ef8ad0aad4c31160606d583f6372c86e576d7b
likaishuai <yunya0221@163.com>
2019-06-29 08:34:51 +0800

    vue first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+      <h3>this is a test page</h3>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
